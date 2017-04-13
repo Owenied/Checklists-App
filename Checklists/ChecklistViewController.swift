a10b6df343fbcb2add660ffb29ac18a70f7ddcfc
Owen Duignan <owenied@me.com>
2017-04-13 12:21:15 -0500

    Add sample data and checkmark capability

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -22,7 +22,7 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView,
@@ -30,7 +30,35 @@ class ChecklistViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
@@ -38,5 +66,6 @@ class ChecklistViewController: UITableViewController {
 
 // To-do List:
 // -----------
-// 1. Add tableview controller and set up cell design with reuse identifier
-// 2.
+// 1. Add sample data
+// 2. Add checkmark capability
+// 3.
